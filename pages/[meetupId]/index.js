f7fa340b4a8c6b49226a91532167a86a83139589
김예지 <fpssj@naver.com>
2024-03-23 03:00:50 +0900

    ✨ 커스텀 컴포넌트 추가하기 & CSS 모듈을 통한 스타일링

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="FirstMeetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    ></MeetupDetail>
+  );
+}
+export default MeetupDetails;
